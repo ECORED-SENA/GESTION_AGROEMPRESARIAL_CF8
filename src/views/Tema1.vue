0645650815c8c318d3399e65a9aa96284c17d35a
maronita <maronita@users.noreply.github.com>
2022-09-14 12:13:08 -0500

    videos

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -175,8 +175,8 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/vE77y1o8DWY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
 
 
     Separador 
